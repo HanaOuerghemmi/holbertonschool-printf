7cd040da0d24a7a6b334648b3e063a14b5099b8b
ayaaribi123 <ayaaribi11@gmail.com>
2022-07-07 12:48:37 +0200

    main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+typedef struct {
+char op;
+char *(*f)(va_list arg);
+} symbole;
+int _putchar(char c);
+void print_char(va_list args);
+void print_string(va_list args);
+void print_op(va_list args);
+int _isdigit(int c);
